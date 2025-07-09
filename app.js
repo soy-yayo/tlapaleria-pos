c702497be0baafb744c23383f70b1f926d35c21f
EduardoMV <yayomar211@gmail.com>
2025-07-09 09:27:24 -0600

    Prueba de conexion db

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const pool = require('./src/db');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API funcionando'));
+
+app.get('/productos', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM productos');
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
